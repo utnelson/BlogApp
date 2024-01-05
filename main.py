7ea8dac3f9bdb3a7b88437bfe67dc637938e4c99
Utnelson <74117256+utnelson@users.noreply.github.com>
2024-01-05 16:53:24 +0100

    Enabled Environment Variables, Debug Mode to False, Create Procfile.c

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, CreateUserForm, LoginForm, CommentForm
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQL_URI")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -238,4 +239,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
