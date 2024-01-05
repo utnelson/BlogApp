cdf8afe5fe2da5ad3297face1272f2b4cc31e880
Utnelson <74117256+utnelson@users.noreply.github.com>
2024-01-05 16:55:45 +0100

    Enabled Environment Variables, Debug Mode to False, Create Procfile.c

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
@@ -12,12 +13,12 @@ from forms import CreatePostForm, CreateUserForm, LoginForm, CommentForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQL_URI")
 db = SQLAlchemy()
 db.init_app(app)
 
