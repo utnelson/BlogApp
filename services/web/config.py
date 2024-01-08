9bd7e5582405bc1feba4bef05c79d7d7492bb212
Utnelson <74117256+utnelson@users.noreply.github.com>
2024-01-08 15:26:48 +0100

    Docker Init

---
+++ services/web/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
